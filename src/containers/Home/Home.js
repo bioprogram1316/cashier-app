04e6936631bc79e30c063df14c6ad1418773404e
bioprogram1316 <cabrams1316@gmail.com>
2019-10-28 11:51:24 -0400

    Homepage Updates

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
+import classes from './Home.module.css';
 import Button from '../../components/UI/Button/Button';
+import OrderHistoryPic from '../../assets/images/cashier-app-past-orders.png';
+import AddItemPic from '../../assets/images/cashier-app-add-items.png';
+import CurrentOrderPic from '../../assets/images/cashier-app-new-order.png';
+import CurrentOrderBottomPic from '../../assets/images/cashier-app-new-order-bottom.png';
+import OrderSummaryPic from '../../assets/images/cashier-app-order-summary.png';
+import ManualAddPic from '../../assets/images/cashier-app-manual-add.png';
 
 const home = (props) => {
 
@@ -12,13 +19,39 @@ return (
     <div>
         <h1>Premier Cashier Application</h1>
         <Button btnType="Success" clicked={loginRegisterHandler}>Log In / Register New User</Button>
-        <h3>Features of the App</h3>
-        <ul>
+        <h2 style={{textDecoration: "underline"}}>Cashier App Features:</h2>
+        <ul className={classes.FeaturesList}>
             <li>Easily import your own inventory list</li>
             <li>Process in-person and online orders</li>
+            <div className={classes.Row}>
+                <div className={classes.TwoColumn}>
+                    <img className={classes.Image} src={CurrentOrderPic} alt="Current Order" />
+                </div>
+                <div className={classes.TwoColumn}>
+                    <img className={classes.Image} src={CurrentOrderBottomPic} alt="Current Order Bottom" />
+                </div>
+            </div>
+            <li>Add items to your order in various formats including scanning</li>
+            <div className={classes.Row}>
+                <div className={classes.OneColumn}>
+                    <img className={classes.Image} src={AddItemPic} alt="Add Item" />
+                </div>
+            </div>
             <li>User Authentication: Users only access their own orders</li>
+            <div className={classes.Row}>
+                <div className={classes.OneColumn}>
+                    <img className={classes.Image} src={OrderHistoryPic} alt="Order History" />
+                </div>
+            </div>
             <li>Simple, Easy to Use Design</li>
-            <p>Pics and Stuff</p>
+            <div className={classes.Row}>
+                <div className={classes.TwoColumn}>
+                    <img className={classes.Image} src={ManualAddPic} alt="Manual Add" />
+                </div>
+                <div className={classes.TwoColumn}>
+                    <img className={classes.Image} src={OrderSummaryPic} alt="Order Summary" />
+                </div>
+            </div>
         </ul>
         <Button btnType="Success" clicked={loginRegisterHandler}>Log In / Register New User</Button>
     </div>
