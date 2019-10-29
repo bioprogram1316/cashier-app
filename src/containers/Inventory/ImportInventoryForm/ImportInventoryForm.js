5f777d2c8927bdaadf93ff436812c194a39e0ce7
bioprogram1316 <cabrams1316@gmail.com>
2019-10-29 11:13:17 -0400

    Added Prelim Import Form

---
+++ src/containers/Inventory/ImportInventoryForm/ImportInventoryForm.js
@@ -0,0 +1,157 @@
+import React, {Component} from 'react';
+
+import {updateObject, checkValidity} from '../../../shared/utility';
+import Input from '../../../components/UI/Input/Input';
+import Button from '../../../components/UI/Button/Button';
+import axios from '../../../axios-orders';
+
+
+class ImportInventoryForm extends Component {
+    state = {
+        importData: {
+            title: "",
+            file: null,
+            description: ""
+        },
+        uploadJson: false,
+        uploadCsv: false,
+        uploadSql: false,
+        importForm: {
+            importName: {
+                elementType: 'input',
+                label: 'Import Name',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Name Your Import'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false
+            },
+            fileType: {
+            elementType: 'select',
+            label: 'Select Import File Type',
+            elementConfig: {
+                options: [
+                    {value: 'json', displayValue: 'JSON'},
+                    {value: 'sql', displayValue: 'SQL'},
+                    {value: 'csv', displayValue: 'CSV'}
+                ]
+            },
+            value: 'csv',
+            validation: {
+                required: true
+            },
+            valid: true
+            },
+            importDescription: {
+                elementType: 'textarea',
+                label: 'Import Description (optional)',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Import Description'
+                },
+                value: '',
+                validation: {
+                    required: false,
+                },
+                valid: true,
+                touched: false
+            }
+        },
+        formIsValid: false
+    }
+
+    uploadJson = () => {
+
+    }
+
+    uploadCsv = () => {
+
+    }
+
+    uploadSql = () => {
+
+    }
+
+    uploadFormHandler = (event) => {
+        event.preventDefault();
+
+        const formData = {};
+        for (let formElementIdentifier in this.state.importForm) {
+            formData[formElementIdentifier] = this.state.importForm[formElementIdentifier].value;
+        }
+
+        // const import = {
+        //     orderId: this.props.orderId,
+        //     orderItems: this.props.orderItems,
+        //     // in a production app you should recalculate the price on the server
+        //     totalPrice: this.props.totalPrice,
+        //     orderData: formData,
+        //     userId: this.props.userId
+        // }
+
+        // this.props.onOrderItems(order, this.props.token);
+        // this.props.checkoutComplete();
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+               
+        const updatedFormElement = updateObject(this.state.importForm[inputIdentifier], {
+            value: event.target.value,
+            valid: checkValidity(event.target.value, this.state.importForm[inputIdentifier].validation),
+            touched: true
+        }); 
+        const updatedImportForm = updateObject(this.state.importForm, {
+            [inputIdentifier]: updatedFormElement
+        });
+ 
+        let formIsValid = true;
+        for (let inputIdentifier in updatedImportForm) {
+            // the form is valid if the specific element is valid and the general formIsValid is still true
+            formIsValid = updatedImportForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({importForm: updatedImportForm, formIsValid: formIsValid});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for (let key in this.state.importForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.importForm[key]
+            });
+        }
+
+        let form = (
+            <form onSubmit={this.uploadFormHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        label={formElement.config.label} 
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation} // validates only if the object has a validation check
+                        touched={formElement.config.touched} // says whether the input line has been touched by the user
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                    />
+                ))}
+                <Button btnType="Success" disabled={!this.state.formIsValid}>IMPORT INVENTORY</Button>
+            </form>);
+
+        return (
+            <div>
+                <h2>Inventory Upload Form</h2>
+                {form}
+            </div>
+        );
+    }
+}
+
+export default ImportInventoryForm;
--- src/containers/Inventory/Inventory.js
+++ src/containers/Inventory/Inventory.js
@@ -1,18 +1,81 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import axios from '../../axios-orders';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Button from '../../components/UI/Button/Button';
+import Modal from '../../components/UI/Modal/Modal';
+import ImportInventoryForm from './ImportInventoryForm/ImportInventoryForm';
 
 class Inventory extends Component {
     state = {
-        inventory: [{}]
+        importComplete: false,
+        importing: true
     }
+    // componentDidMount() {
+        // fetch inventory associated with this user 
+        // give option to import new inventory
+        // if there is no associated inventory, only option should be to import
+    //     this.props.onFetchInventory(this.props.token, this.props.userId);
+    // }
+
+    importCompleteHandler = () => {
+        this.setState({importComplete: false});
+        this.setState({importing: false});
+    };
+
+    importHandler = () => {
+        this.setState({importing: true});
+    };
+
+    importCancelledHandler = () => {
+    };
 
     render () {
+        // console.log(this.props);
+         let inventory = <Spinner />; 
+        // if (!this.props.loading) {
+        // inventory = this.props.orders.map(item => (
+        //         <li><InventoryItem 
+        //            key={order.id}
+        //            orderId={order.orderId}
+        //            orderItems={order.orderItems}
+        //            price={order.totalPrice}
+        //        /></li>
+        //     ))
+        // }
+        // console.log(inventory);
+
+
         return (
             <div>
-                <button>Import Inventory</button>
+                <Modal show={this.state.importing} closed={this.importCompleteHandler} >
+                    <ImportInventoryForm />
+                </Modal>
+                <Button >Import Inventory</Button>
                 <p>Inventory List:</p>
+                <ul>{inventory}</ul>
             </div>
         );
     }
 }
 
-export default Inventory;
+const mapStateToProps = state => {
+    return {
+        //inventory: state.order.orders,
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
+    };
+};
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         //onFetchInventory: (token, userId) => dispatch(actions.fetchInventory(token, userId)),
+//         //onImportInventory: (importData, token) => dispatch(actions.importInventory(importData, token))
+//     };
+// };
+
+//export default connect(mapStateToProps, mapDispatchToProps)(Inventory, axios);
+export default connect(mapStateToProps)(Inventory, axios);
